8cb8e6f0afafff0e3071bffb7815385abf0f2d36
Shreevathsa <shreevathsa99@gmail.com>
2019-06-02 22:14:04 +0530

    commits from june 2

--- chapter_9/myTest/printAdd.cpp
+++ chapter_9/myTest/printAdd.cpp
@@ -4,8 +4,14 @@
 using namespace std;
 
 int main(){
-	char a;
-	char b;
-	cout << &a << endl << &b;
+	char  a = 'H';
+	char  b = 'a';
+	char* p; 
+
+	p = &a;
+	cout << *p << endl;
+
+	p = &b;
+	cout << *p << endl;
 	return 0;
 }
